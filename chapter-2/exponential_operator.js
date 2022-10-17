190ffb859f924c42aae1cd5921f4f7df8716d112
Marina <marina.marqueso@outlook.com>
2022-10-17 18:36:16 -0300

    feat: read modules, operators and getters setters

---
+++ chapter-2/exponential_operator.js
@@ -0,0 +1,6 @@
+let r = 5
+
+// const area = 3.14 * r * r
+// const area = 3.14 * Math.pow(r, 2)
+const area = 3.14 * (r ** 2)
+console.log(area)
---
+++ chapter-2/modules.js
@@ -0,0 +1,8 @@
+// import { circleArea, squareArea } from "./calc_area_modules.js";
+// console.log(circleArea(2), squareArea(2))
+
+// import { circleArea as circle, squareArea as square } from "./calc_area_modules.js";
+// console.log(circle(2), square(2))
+
+import * as area from "./calc_area_modules.js";
+console.log(area.circleArea(2), area.squareArea(2))
