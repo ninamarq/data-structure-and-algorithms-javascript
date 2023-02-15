46bc2f14025673cac1c11cd8b4728281e6e7abef
Marina <marina.marqueso@outlook.com>
2023-02-15 13:56:49 -0300

    feat: add iterator methods, stoped before fill method

--- chapter-3/iteration-functions.js
+++ chapter-3/iteration-functions.js
@@ -1,4 +1,4 @@
-const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+export const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
 const isEven = x => x % 2 === 0
 
 // every method
@@ -26,3 +26,26 @@ console.log(filter)
 // reduce
 const reduce = numbers.reduce((previous, current) => previous + current)
 console.log(reduce)
+
+// for ... of
+for (const n of numbers) {
+  console.log(n % 2 === 0 ? 'even': 'odd')
+}
+
+// @@iterator
+let iterator = numbers[Symbol.iterator]()
+console.log(iterator.next().value)
+console.log(iterator.next().value)
+console.log(iterator.next().value)
+
+for (const n of iterator) {
+  console.log(n)
+}
+
+// entries, values & keys
+let aEntries = numbers.entries()
+console.log(aEntries.next().value)
+
+let aKeys = numbers.keys()
+// done representa se chegou ao final da array ou não - done represents if array is over, boolean value
+console.log(aKeys.next())
