9468e0ddddeb0934ed6445f81b734b1511bcb2dc
Marina Marques <marina.marqueso@outlook.com>
2023-03-29 22:34:23 -0300

    feat: file was not completed, adding methods to stack

--- chapter-4/stack-based-array.js
+++ chapter-4/stack-based-array.js
@@ -2,8 +2,28 @@ class Stack {
   constructor() {
     this.items = [];
   }
-
+  // add element as last element from stack
   push(element) {
     this.items.push(element)
+  }
+  // remove last element - representing the LIFO principle. the first is the last, and the last is the first
+  pop() {
+    return this.items.pop()
+  }
+  // searching the last element added, the peek of stack
+  peek() {
+    return this.items[this.items.length - 1]
+  }
+  // checking if stack is empty
+  isEmpty() {
+    return this.items.length === 0
+  }
+  // checking stack size
+  size() {
+    return this.items.length
+  }
+  // cleaning stack
+  clear() {
+    this.items = []
   }
 }
