6b00d59ac41e8f3d997dec6a009a12a4b1e4eb7d
Marina Marques <marina.marqueso@outlook.com>
2023-04-30 22:14:57 -0300

    feat: add pop algorithm

--- chapter-4/stack-class.js
+++ chapter-4/stack-class.js
@@ -17,10 +17,22 @@ class Stack {
   isEmpty() {
     return this.count === 0
   }
+
+  // removing last element from object (items)
+  pop() {
+    if (this.isEmpty()) return undefined
+
+    this.count--
+    const result = this.items[this.count]
+    delete this.items[this.count]
+    return result
+  }
 }
 
 const stack = new Stack
 stack.push(8)
 stack.push(4)
+
+stack.pop()
 
 console.log(stack.count, stack.items)
