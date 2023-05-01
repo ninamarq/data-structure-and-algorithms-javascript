68143440e5c2a3742720eb638578d04e67093c51
Marina Marques <marina.marqueso@outlook.com>
2023-04-30 22:06:38 -0300

    feat: add size and is empty methods

--- chapter-4/stack-class.js
+++ chapter-4/stack-class.js
@@ -8,4 +8,19 @@ class Stack {
     this.items[this.count] = element
     this.count++
   }
+
+  // verifying if items is empty or its size
+  size() {
+    return this.count
+  }
+
+  isEmpty() {
+    return this.count === 0
+  }
 }
+
+const stack = new Stack
+stack.push(8)
+stack.push(4)
+
+console.log(stack.count, stack.items)
