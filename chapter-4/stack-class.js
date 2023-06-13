a78e2d6a259dfd4dc7933c69fc423bcaeec07880
Marina Marques <marina.marqueso@outlook.com>
2023-06-13 20:14:00 -0300

    finish stack

--- chapter-4/stack-class.js
+++ chapter-4/stack-class.js
@@ -1,37 +1,37 @@
 class Stack {
   constructor() {
-    this.count = 0
-    this.items = {}
+    this._count = 0
+    this._items = {}
   }
   // push
   push(element) {
-    this.items[this.count] = element
-    this.count++
+    this._items[this._count] = element
+    this._count++
   }
 
   // verifying if items is empty or its size
   size() {
-    return this.count
+    return this._count
   }
 
   isEmpty() {
-    return this.count === 0
+    return this._count === 0
   }
 
   // removing last element from object (items)
   pop() {
     if (this.isEmpty()) return undefined
 
-    this.count--
-    const result = this.items[this.count]
-    delete this.items[this.count]
+    this._count--
+    const result = this._items[this._count]
+    delete this._items[this._count]
     return result
   }
 
   peek() {
     if (this.isEmpty()) return undefined
 
-    return this.items[this.count - 1]
+    return this._items[this._count - 1]
   }
 
   clear() {
@@ -41,10 +41,10 @@ class Stack {
   toString() {
     if (this.isEmpty()) return ''
 
-    let objString = `${this.items[0]}`
+    let objString = `${this._items[0]}`
 
-    for (let i = 1; i < this.count; i++) {
-      objString = `${objString},${this.items[i]}`
+    for (let i = 1; i < this._count; i++) {
+      objString = `${objString},${this._items[i]}`
     }
 
     return objString
@@ -59,4 +59,7 @@ stack.push(1)
 
 stack.pop()
 
-console.log(stack.toString())
+// console.log(stack.toString())
+
+// getting public methods
+console.log({names: Object.getOwnPropertyNames(stack)})
