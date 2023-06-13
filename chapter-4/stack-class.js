48de0527064ddf23cff1f7cc658ecc15591c727a
Marina Marques <marina.marqueso@outlook.com>
2023-06-13 19:48:08 -0300

    feat: add to string method, chapter 4

--- chapter-4/stack-class.js
+++ chapter-4/stack-class.js
@@ -1,7 +1,7 @@
 class Stack {
   constructor() {
     this.count = 0
-    this.items = []
+    this.items = {}
   }
   // push
   push(element) {
@@ -27,12 +27,36 @@ class Stack {
     delete this.items[this.count]
     return result
   }
+
+  peek() {
+    if (this.isEmpty()) return undefined
+
+    return this.items[this.count - 1]
+  }
+
+  clear() {
+    while (!this.isEmpty()) this.pop()
+  }
+
+  toString() {
+    if (this.isEmpty()) return ''
+
+    let objString = `${this.items[0]}`
+
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`
+    }
+
+    return objString
+  }
 }
 
 const stack = new Stack
 stack.push(8)
 stack.push(4)
+stack.push(7)
+stack.push(1)
 
 stack.pop()
 
-console.log(stack.count, stack.items)
+console.log(stack.toString())
