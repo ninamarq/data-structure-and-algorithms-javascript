5b5ea168cb60ca799152a9f2b14988b82cb1d6eb
Marina Marques <marina.marqueso@outlook.com>
2025-01-06 09:00:57 -0300

    feat: apply removeAt, getElementAt and insert methods

--- chapter-6/linked-list.js
+++ chapter-6/linked-list.js
@@ -36,19 +36,47 @@ export default class LinkedList {
       if (index === 0) {
         this.head = current.next;
       } else {
-        let previous;
-
-        for (let i = 0; i < index; i++) {
-          previous = current;
-          current = current.next;
-        }
-        previous.next = current.next;
+        const previous = this.getElementAt(index - 1)
+        current = previous.next
+        previous.next = current.next
       }
       this.count--;
       return current.element;
     }
 
     return undefined;
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) {
+      let node = this.head
+      for (let i = 0; i < index && node != null; i++) {
+        node = node.next
+      }
+
+      return node
+    }
+    
+    return undefined
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) {
+      const node = new Node(element)
+      if (index === 0) {
+        const current = this.head
+        node.next = current
+        this.head = node
+      } else {
+        const previous = this.getElementAt(index - 1)
+        const current = previous.next
+        node.next = current
+        previous.next = node
+      }
+      this.count++
+      return true
+    }
+    return false
   }
 }
 
